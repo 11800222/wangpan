0f195af8a7a95f07e87cfaab7c83d3209185e839
11800222 <545423690@qq.com>
2017-07-05 15:09:42 +0800

    d

--- wanpan/src/basedao/BaseDao.java
+++
@@ -1,6 +0,0 @@
-package basedao;
-
-public interface BaseDao {
-    void save(Object object);
-    Object get(String ID);
-}
--- wanpan/src/util/StringUtil.java
+++
@@ -1,7 +0,0 @@
-package util;
-
-public class StringUtil {
-	public static boolean isNotEmpty(String s){
-        return s!=null && s.trim().length()>0;
-    }
-}
